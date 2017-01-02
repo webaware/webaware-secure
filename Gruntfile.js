0f53dd67a7f82cbb3739082172f463b4d1e2993b
Ross McKay <rmckay@webaware.com.au>
2017-01-02 13:48:35 +1100

    add grunt config for making .zip

---
+++ Gruntfile.js
@@ -0,0 +1,54 @@
+module.exports = function (grunt) {
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON("package.json"),
+
+		clean: [ "dist/**" ],
+
+		copy: {
+			main: {
+				files: [
+					{
+						src: ["./**", "!./node_modules/**", "!./Gruntfile.js", "!./package.json"],
+						dest: "dist/<%= pkg.name %>/"
+					}
+				]
+			}
+		},
+
+		compress: {
+			options: {
+				archive: "./dist/<%= pkg.name %>-<%= pkg.version %>.zip",
+				mode: "zip"
+			},
+			all: {
+				files: [{
+					expand: true,
+					cwd: "./dist/",
+					src: [ "<%= pkg.name %>/**" ]
+				}]
+			}
+		},
+
+		jshint: {
+			all: [
+				"Gruntfile.js",
+				"js/*.js",
+				"!js/*.min.js"
+			],
+			options: {
+				jshintrc: ".jshintrc",
+				force: true
+			}
+		}
+
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks("grunt-contrib-compress");
+	grunt.loadNpmTasks("grunt-contrib-copy");
+	grunt.loadNpmTasks("grunt-contrib-jshint");
+
+	grunt.registerTask("release", ["clean","copy","compress"]);
+
+};
